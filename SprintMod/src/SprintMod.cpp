cfeea3d5d3da137d59673df04cb3556b89048c70
XuanDiNa <160459294+XuanDiNa@users.noreply.github.com>
2026-02-25 00:48:20 +0800

    Add files via upload
    Fixed the issue that would affect the Ctrl key outside of the game.

--- SprintMod/src/SprintMod.cpp
+++ SprintMod/src/SprintMod.cpp
@@ -10,31 +10,57 @@ bool g_autoCtrlHeld = false;
 HANDLE g_thread = nullptr;
 bool g_running = false;
 
+bool IsMinecraftActive() {
+    char title[256];
+    HWND hwnd = GetForegroundWindow();
+    GetWindowTextA(hwnd, title, sizeof(title));
+    return strstr(title, "Minecraft") != nullptr;
+}
+
+bool IsInGame() {
+    HWND hwnd = GetForegroundWindow();
+    char title[256];
+    GetWindowTextA(hwnd, title, sizeof(title));
+    
+    if (strstr(title, "Minecraft") == nullptr) return false;
+    if (strstr(title, "Play") != nullptr) return false;
+    if (strstr(title, "Settings") != nullptr) return false;
+    if (strstr(title, "Options") != nullptr) return false;
+    if (strstr(title, "Chat") != nullptr) return false;
+    if (strstr(title, "World") != nullptr) return false;
+    if (strstr(title, "Server") != nullptr) return false;
+    if (strstr(title, "Menu") != nullptr) return false;
+    
+    return true;
+}
+
 DWORD WINAPI InputThread(LPVOID) {
     while (g_running) {
-        bool ctrlPressed = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
-        
-        if (ctrlPressed && !g_autoCtrlHeld) {
-            g_autoSprintEnabled = !g_autoSprintEnabled;
-            g_autoCtrlHeld = true;
-            char buf[256];
-            sprintf_s(buf, "[AutoSprint] Mode: %s\n", g_autoSprintEnabled ? "ON" : "OFF");
-            OutputDebugStringA(buf);
-        }
-        
-        if (!ctrlPressed && g_autoCtrlHeld) {
-            g_autoCtrlHeld = false;
-        }
-        
-        if (g_autoSprintEnabled) {
-            bool wPressed = (GetAsyncKeyState('W') & 0x8000) != 0;
+        if (IsInGame()) {
+            bool ctrlPressed = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
             
-            if (wPressed && !g_autoCtrlHeld) {
-                keybd_event(VK_CONTROL, 0, 0, 0);
+            if (ctrlPressed && !g_autoCtrlHeld) {
+                g_autoSprintEnabled = !g_autoSprintEnabled;
                 g_autoCtrlHeld = true;
-            } else if (!wPressed && g_autoCtrlHeld) {
-                keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+                char buf[256];
+                sprintf_s(buf, "[AutoSprint] Mode: %s\n", g_autoSprintEnabled ? "ON" : "OFF");
+                OutputDebugStringA(buf);
+            }
+            
+            if (!ctrlPressed && g_autoCtrlHeld) {
                 g_autoCtrlHeld = false;
+            }
+            
+            if (g_autoSprintEnabled) {
+                bool wPressed = (GetAsyncKeyState('W') & 0x8000) != 0;
+                
+                if (wPressed && !g_autoCtrlHeld) {
+                    keybd_event(VK_CONTROL, 0, 0, 0);
+                    g_autoCtrlHeld = true;
+                } else if (!wPressed && g_autoCtrlHeld) {
+                    keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+                    g_autoCtrlHeld = false;
+                }
             }
         }
         
